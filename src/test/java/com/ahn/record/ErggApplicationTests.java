90abf0770af425ed62f00d61b03880a4c399a9af
AHN <dsfs3975@naver.com>
2024-01-22 09:23:44 +0900

    [안재원]  - 최초 파일 추가  - 설정 파일 수

---
+++ src/test/java/com/ahn/record/ErggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahn.record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
